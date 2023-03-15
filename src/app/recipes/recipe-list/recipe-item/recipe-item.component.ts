707cf114d8616907a48bf5e276300b07f9c4fb35
hritex <neon147369@gmail.com>
2023-03-15 17:25:05 +0530

    Passing Data with Property Binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 import { Recipe } from '../../recipe.modal';
 
 @Component({
@@ -7,8 +8,6 @@ import { Recipe } from '../../recipe.modal';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test description', 
-    'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_2240,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')
-  ];
+  @Input() recipe: Recipe | any;
+  
 }
