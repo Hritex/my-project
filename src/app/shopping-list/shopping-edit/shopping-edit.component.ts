f150c657874f4d7799c633c4b0a3f41763bf9ca3
hritex <neon147369@gmail.com>
2023-03-19 15:38:54 +0530

    My Ingredient Addition to Shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.modal';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInput! : ElementRef;
+  @ViewChild('amountInput') amountInput! : ElementRef;
+  ingredient: Ingredient[] = [];
+
+  onAdd(newNameInput: HTMLInputElement, newAmountInput: HTMLInputElement) {
+    newNameInput = this.nameInput.nativeElement.value;
+    newAmountInput = this.amountInput.nativeElement.value;
+    console.log(newNameInput, newAmountInput);
+    this.ingredient.push({'name': newNameInput, 'amount': newAmountInput});
+  }
 
 }
