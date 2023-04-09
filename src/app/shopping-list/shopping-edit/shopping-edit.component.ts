479e46986636bfb6807dd2a098b9dda42d402ebd
hritex <neon147369@gmail.com>
2023-04-09 20:16:00 +0530

    Adding the Shopping list service.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 import { Ingredient } from 'src/app/shared/ingredient.modal';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,15 +11,16 @@ import { Ingredient } from 'src/app/shared/ingredient.modal';
 export class ShoppingEditComponent {
   @ViewChild('nameInput') nameInputRef: ElementRef | any;
   @ViewChild('amountInput') amountInputRef: ElementRef | any;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>(); 
   /* Or can also use 
   @Output() ingredientAdded = new EventEmitter<{name: string, amount: number}>();*/
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   onAddItem() {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value; // These values are not going to change so "const"
     const newIngredient = new Ingredient(ingName, ingAmount); // Should use const if you are not going to change the value
-    this.ingredientAdded.emit(newIngredient);
+    this.shoppingListService.addIngredient(newIngredient);
   }
 
 }
