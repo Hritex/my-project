f0e065a095a8e3d985aa666f6c8a8c8e082cc2bb
hritex <neon147369@gmail.com>
2023-04-09 19:39:02 +0530

    Managing Recipes in a Recipe Service.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,14 @@
+import { Recipe } from "./recipe.modal";
+
 export class RecipeService {
-    
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test description', 
+        'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_2240,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'),
+        new Recipe('Another Test Recipe', 'This is simply a test description', 
+        'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_2240,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')
+      ];
+
+    getRecipes() {
+        return this.recipes.slice(); // we only get a capy of the recipe array, since it is private using slice method 
+    }
 }
