6b34c2d1548d1355961972a78054b678cac905f3
hritex <neon147369@gmail.com>
2023-04-09 20:41:29 +0530

    Adding Ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,31 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.modal";
+import { Ingredient } from "../shared/ingredient.modal";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test description', 
-        'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_2240,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test description', 
-        'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_2240,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')
+        new Recipe(
+            'Tasty Schnitzel', 
+            'A super-tasty Schnitzel - just awesome!',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1200px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+            [
+                new Ingredient('Meat',1),
+                new Ingredient('French Fries', 20)
+            ]
+        ),
+        new Recipe(
+            'Big Burger',
+            'A large sized Burger',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5cKjbFpdJGH5t7p_InqdY3yyOHLTZDmCp1Q&usqp=CAU',
+            [
+                new Ingredient('Buns',2),
+                new Ingredient('Potatoes', 2),
+                new Ingredient('Onion', 1),
+                new Ingredient('Capsicum', 1)
+            ]
+        )
       ];
 
     getRecipes() {
